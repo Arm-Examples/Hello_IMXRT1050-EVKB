d2baf1e1c2f52135237d1249a02d2483fdb7cf27
evangelos.ganotopoulos@arm.com <Eve07@Git>
2024-10-14 18:59:15 +0200

    Corrected some errors

--- RTE/_Release_IMXRT1050-EVKB/RTE_Components.h
+++ RTE/_Release_IMXRT1050-EVKB/RTE_Components.h
@@ -15,13 +15,6 @@
  */
 #define CMSIS_device_header "fsl_device_registers.h"
 
-/* ARM::CMSIS-Compiler:STDOUT:Custom@1.1.0 */
-#define RTE_CMSIS_Compiler_STDOUT                /* CMSIS-Compiler STDOUT */
-#define RTE_CMSIS_Compiler_STDOUT_Custom         /* CMSIS-Compiler STDOUT: Custom */
-/* ARM::CMSIS:RTOS2:Keil RTX5&Source@5.9.0 */
-#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
-#define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
-#define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
 /* NXP::Board Support:SDK Drivers:evkbimxrt1050@2.0.1 */
 #ifndef XIP_EXTERNAL_FLASH
 #define XIP_EXTERNAL_FLASH 1
